dc57bd08adf5fab2af0e7c4f9546d0906923fb11
jeffpeng3 <jeffpeng2012@gmail.com>
2025-01-16 21:59:13 +0800

    feat: use DB instead of maintain dict by self

--- Community.PowerToys.Run.Plugin.Its-MyPic/Main.cs
+++ Community.PowerToys.Run.Plugin.Its-MyPic/Main.cs
@@ -14,14 +14,13 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Collections.Specialized;
+using Wox.Infrastructure;
 
 namespace Community.PowerToys.Run.Plugin.Its_MyPic
 {
 	public class Main : IPlugin
 	{
-		readonly JsonSerializerOptions options = new() { PropertyNameCaseInsensitive = true };
-		private readonly Dictionary<string, SubtitleData> subtitleDatas = [];
-		private readonly HttpClient client = new();
+		private readonly Data DB = new();
 
 		public static string PluginID => "048FCB4CE3034FD9ACD9486F9FAB1F9E";
 		public string Name => "Its-MyPic";
@@ -37,63 +36,36 @@ namespace Community.PowerToys.Run.Plugin.Its_MyPic
 		public List<Result> Query(Query query)
 		{
 			List<Result> results = [];
-
 			var search = query.Search.ToLower();
-
-			// filt out the subtitleDatas that match the search
-
-			var filteredDatas = _cache.GetOrAdd(search, () => subtitleDatas.Where(data => data.Key.Contains(search)).Take(5).ToList());
-
-			_ = PrepareImage(filteredDatas);
-
-			foreach (var filteredData in filteredDatas)
+			var subtitles = DB.GetSubtitleDatas(search);
+			foreach (var subtitle in subtitles)
 			{
-				var subtitleData = filteredData.Value;
-				var IcoPath = $"{PluginDirectory}/Images/{subtitleData.file_name}";
-				if (!File.Exists(IcoPath))
-				{
-					IcoPath = IconPath;
-				}
+				var IcoPath = $"{PluginDirectory}/Images/{subtitle.FileName}";
+				var r = StringMatcher.FuzzySearch(query.Search, subtitle.Text);
 				results.Add(new Result
 				{
 					QueryTextDisplay = search,
 					IcoPath = IcoPath,
-					Title = subtitleData.name,
-					SubTitle = $"{subtitleData.description}",
+					Title = subtitle.Text,
+					TitleHighlightData = r.MatchData,
+					Score = r.Score,
+					//SubTitle = $"{subtitleData.description}",
 					//ToolTipData = new ToolTipData("Title", "Text"),
 					Action = _ =>
 					{
 						var list = new StringCollection
 						{
-							$"{PluginDirectory}/Images/{subtitleData.file_name}"
+							$"{PluginDirectory}/Images/{subtitle.FileName}"
 						};
 						Clipboard.SetFileDropList(list);
 						return true;
 					},
-					ContextData = subtitleData.name,
+					ContextData = subtitle,
 				});
 			}
 			return results;
 		}
 
-		private async Task PrepareImage(List<KeyValuePair<string, SubtitleData>> filteredDatas)
-		{
-			IEnumerable<Task> tasks = filteredDatas.Select(d => DownloadImage(d.Value));
-			await Task.WhenAll(tasks);
-		}
-
-		private async Task DownloadImage(SubtitleData subtitleData)
-		{
-			var file_name = subtitleData.file_name;
-			var file_path = $"{PluginDirectory}/Images/{file_name}";
-			if (!File.Exists(file_path))
-			{
-				var url = $"https://drive.miyago9267.com/d/file/img/mygo/{file_name}";
-				using HttpResponseMessage message = await client.GetAsync(url);
-				var stream = await message.Content.ReadAsByteArrayAsync();
-				await File.WriteAllBytesAsync(file_path, stream);
-			}
-		}
 
 		public void Init(PluginInitContext context)
 		{
@@ -101,13 +73,8 @@ namespace Community.PowerToys.Run.Plugin.Its_MyPic
 			Context.API.ThemeChanged += OnThemeChanged;
 			UpdateIconPath(Context.API.GetCurrentTheme());
 
-			_cache = new CachingService();
+			//_cache = new CachingService();
 
-			var datas = JsonSerializer.Deserialize<List<SubtitleData>>(File.ReadAllText($"{PluginDirectory}/data.json"), options);
-			foreach (var subtitleData in datas)
-			{
-				subtitleDatas[subtitleData.name.ToLower()] = subtitleData;
-			}
 		}
 
 		private void UpdateIconPath(Theme theme) => IconPath = theme == Theme.Light || theme == Theme.HighContrastWhite ? "Images/its_mypic.light.png" : "Images/its_mypic.dark.png";
