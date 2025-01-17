c746e472764de5d78b28986abbb4eadb4dd48c89
jeffpeng <jeffpeng2012@gmail.com>
2025-01-17 14:54:26 +0800

    feat: imporve install script

--- setup.bat
+++ setup.bat
@@ -1,8 +1,26 @@
 @echo off
-set new_folder=%LocalAppData%\Microsoft\PowerToys\PowerToys Run\Plugins\Its-MyPic
-md "%new_folder%"
+cd /d "%~dp0"
+
+taskkill /F /IM PowerToys*
+ping -n 2 127.0.0.1 >nul
+tasklist | findstr /i "PowerToys.exe" >nul
+if %errorlevel% equ 0 (
+    echo 終止 PowerToys 失敗，請以系統管理員身分重新執行本批次檔。
+    pause
+    exit
+)
+
+set ptPath=%LocalAppData%\PowerToys
+set newFolder=%LocalAppData%\Microsoft\PowerToys\PowerToys Run\Plugins\Its-MyPic
+md "%newFolder%"
+md "%newFolder%\images"
 echo.
-copy * "%new_folder%"
+copy /y * "%newFolder%"
+echo.
+echo 正在重啟 PowerToys...
+
+start /B "window" "%ptPath%\PowerToys.exe"
+
 echo.
 echo 安裝完成
 echo 如有異常請回報給作者
