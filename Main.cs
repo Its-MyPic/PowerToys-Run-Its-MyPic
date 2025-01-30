ce6ebd64c2b199f91c64afab5eeb9b1768dc2ab9
jeffpeng3 <jeffpeng2012@gmail.com>
2025-01-30 22:39:52 +0800

    feat: add updater

--- Main.cs
+++ Main.cs
@@ -7,10 +7,13 @@ using System.IO;
 using Microsoft.PowerToys.Settings.UI.Library;
 using System.Windows.Controls;
 using System.Linq;
+using Community.PowerToys.Run.Plugin.Update;
+using Wox.Infrastructure.Storage;
+using Wox.Plugin.Logger;
 
 namespace Community.PowerToys.Run.Plugin.Its_MyPic
 {
-	public class Main : IPlugin, IDelayedExecutionPlugin, IContextMenu, ISettingProvider, IPluginI18n
+	public class Main : IPlugin, IDelayedExecutionPlugin, IContextMenu, ISettingProvider, IPluginI18n, ISavable
 	{
 		public static string PluginID => "048FCB4CE3034FD9ACD9486F9FAB1F9E";
 		public string Name => "Its-MyPic";
@@ -30,6 +33,13 @@ namespace Community.PowerToys.Run.Plugin.Its_MyPic
 
 		private readonly Data DB = new();
 
+		private PluginJsonStorage<SampleSettings> Storage { get; set; }
+		public void Save() => Storage.Save();
+
+		private SampleSettings Settings { get; set; }
+
+		private PluginUpdateHandler Updater { get; set; }
+		private PluginInitContext Context { get; set; }
 
 		public List<Result> Query(Query query)
 		{
@@ -38,12 +48,34 @@ namespace Community.PowerToys.Run.Plugin.Its_MyPic
 		public List<Result> Query(Query query, bool delayedExecution)
 		{
 			var search = query.Search.ToLower();
-			var results = DB.GetMatchedSubtitleDatas(search, delayedExecution);
+			List<Result> results;
+			if (Updater.IsUpdateAvailable())
+			{
+				Log.Info("Update available", GetType());
+				results = Updater.GetResults();
+			}
+			else
+			{
+				results = DB.GetMatchedSubtitleDatas(search, delayedExecution);
+			}
 			return results;
 		}
 		public void Init(PluginInitContext context)
 		{
 			CopyImage = false;
+			Context = context;
+
+			Storage = new PluginJsonStorage<SampleSettings>();
+			Settings = Storage.Load();
+
+			Log.Info(context.CurrentPluginMetadata.Version, GetType());
+
+			Updater = new PluginUpdateHandler(Settings.Update);
+			Updater.UpdateInstalling += OnUpdateInstalling;
+			Updater.UpdateInstalled += OnUpdateInstalled;
+			Updater.UpdateSkipped += OnUpdateSkipped;
+
+			Updater.Init(Context);
 		}
 
 		public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
@@ -85,6 +117,25 @@ namespace Community.PowerToys.Run.Plugin.Its_MyPic
 		public string GetTranslatedPluginDescription()
 		{
 			throw new System.NotImplementedException();
+		}
+
+
+		private void OnUpdateInstalling(object sender, PluginUpdateEventArgs e)
+		{
+			Log.Info("UpdateInstalling: " + e.Version, GetType());
+		}
+
+		private void OnUpdateInstalled(object sender, PluginUpdateEventArgs e)
+		{
+			Log.Info("UpdateInstalled: " + e.Version, GetType());
+			Context!.API.ShowNotification($"{Name} {e.Version}", "Update installed");
+		}
+
+		private void OnUpdateSkipped(object sender, PluginUpdateEventArgs e)
+		{
+			Log.Info("UpdateSkipped: " + e.Version, GetType());
+			Save();
+			Context?.API.ChangeQuery(Context.CurrentPluginMetadata.ActionKeyword, true);
 		}
 	}
 }
---
+++ Updater.cs
@@ -0,0 +1,19 @@
+﻿using Community.PowerToys.Run.Plugin.Update;
+using Microsoft.PowerToys.Settings.UI.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Community.PowerToys.Run.Plugin.Its_MyPic
+{
+	public class SampleSettings
+	{
+		public PluginUpdateSettings Update { get; set; } = new PluginUpdateSettings { ResultScore = 100 };
+
+		internal IEnumerable<PluginAdditionalOption> GetAdditionalOptions() => Update.GetAdditionalOptions();
+
+		internal void SetAdditionalOptions(IEnumerable<PluginAdditionalOption> additionalOptions) => Update.SetAdditionalOptions(additionalOptions);
+	}
+}
